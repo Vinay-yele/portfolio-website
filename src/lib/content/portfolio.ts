50330bec3c17127a649a575e919f2979265f0f67
vinay yele <147807461+Vinay-yele@users.noreply.github.com>
2024-09-07 18:15:27 +0530

    Update portfolio.ts

--- src/lib/content/portfolio.ts
+++ src/lib/content/portfolio.ts
@@ -7,33 +7,33 @@
 import { StringKeyValueType } from '../types';
 
 export const socialLinks: StringKeyValueType = {
-  facebook: 'https://www.facebook.com/vatsalsinghkv',
-  instagram: 'https://www.instagram.com/vatsalsinghkv',
-  twitter: 'https://twitter.com/vatsalsinghkv',
-  github: 'https://github.com/vatsalsinghkv',
-  linkedin: 'https://www.linkedin.com/in/vatsalsinghkv/',
+  facebook: 'https://www.facebook.com/profile.php?id=100006291974644',
+  instagram: 'https://www.instagram.com/vinay_yele',
+  twitter: 'https://x.com/vinay_yele',
+  github: 'https://github.com/Vinay-yele',
+  linkedin: 'https://www.linkedin.com/in/vinay-yele-08ab08295/',
 };
 
 export const author = {
-  name: 'Vatsal Singh',
-  email: 'vatsalsinghkv@gmail.com',
+  name: 'Vinay Yele',
+  email: 'vinayyele1998@gmail.com',
 };
 
 export const seoData = {
-  title: 'Vatsal Singh | Front-End Developer',
+  title: 'Vinay Yele | Front-End Developer',
   description:
-    'Vatsal Singh is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+    'Vinay Yele is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
   author: author.name,
   image:
     'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
   url: 'https://vatsalsinghkv.vercel.app/',
   keywords: [
-    'Vatsal',
-    'Vatsal Singh',
-    '@vatsalsinghkv',
-    'vatsalsinghkv',
+    'Vinay',
+    'Vinay Yele',
+    '@vinayyelekv',
+    'vinayyekekv',
     'Portfolio',
-    'Vatsal Portfolio ',
-    'Vatsal Singh Portfolio',
+    'Vinay Portfolio ',
+    'Vinay Yele Portfolio',
   ],
 };
